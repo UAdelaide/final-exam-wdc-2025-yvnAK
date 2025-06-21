b0da63af11859e58e57f373ff8902093ee153f33
yvnAK <57645366+yvnAK@users.noreply.github.com>
2025-06-21 07:10:19 +0000

    [automatic commit] 2025-06-21 07:10:19 UTC 59.102.38.160

--- part2/app.js
+++ part2/app.js
@@ -174,13 +174,11 @@ app.post('/login', async (req, res) => {
 
         if (user.role === 'owner') return res.redirect('/owner-dashboard');
         if (user.role === 'walker') return res.redirect('/walker-dashboard');
-    }
-    catch (error){
-        cons
-    }
+    } catch (err) {
+        console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
 
 
-});
+    });
 
 function redirectIfAuth(req, res, next) {
     if (req.session.user) { // ifuser is alerady authenticated
