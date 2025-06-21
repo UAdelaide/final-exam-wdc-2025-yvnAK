5ade6b4f03b6b0605ce4d553707494349524c247
yvnAK <57645366+yvnAK@users.noreply.github.com>
2025-06-21 00:45:52 +0000

    [automatic commit] 2025-06-21 00:45:52 UTC 59.102.38.160

--- part1/app.js
+++ part1/app.js
@@ -165,7 +165,7 @@ app.get('/', async (req, res) => {
         // get users where role = 'walker'
         // list username, number of ratings, rating, completed walks
             // username and rating is local to Users
-            // number of ratings is the number of rows in 
+            // number of ratings is the number of rows in ratings with the 
         const [userRows] = await db.execute();
         res.json(userRows);
     } catch (err) {
