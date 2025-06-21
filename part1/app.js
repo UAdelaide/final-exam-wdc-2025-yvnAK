ba146b9f74e9bf332b54455b10a52e1c65abc5a6
yvnAK <57645366+yvnAK@users.noreply.github.com>
2025-06-21 01:18:52 +0000

    [automatic commit] 2025-06-21 01:18:52 UTC 59.102.38.160

--- part1/app.js
+++ part1/app.js
@@ -144,9 +144,9 @@ app.get('/api/dogs', async (req, res) => {
         // join via foreign key 'owner id'
         const query = `
             select
-                d.name as dogName,
+                d.name as dog_name,
                 d.size,
-                u.username as owName
+                u.username as owner_username
             from Dogs d
             inner join Users u on d.owner_id = u.user_id
         `;
