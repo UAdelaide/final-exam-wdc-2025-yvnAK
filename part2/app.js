de98591e7ba76e401015f7f7e9ede4e0992adac0
yvnAK <57645366+yvnAK@users.noreply.github.com>
2025-06-21 06:50:22 +0000

    [automatic commit] 2025-06-21 06:50:22 UTC 59.102.38.160

--- part2/app.js
+++ part2/app.js
@@ -20,8 +20,8 @@ app.use(express.static(path.join(__dirname, '/public')));
 // GET login
 app.get('/', (req, res) => { res.render('login', { error: null }); });
 app.get('/', redirectIfAuth, (req, res) => {
-    res.render
-}
+    res.render('login', { error: null });
+};
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
