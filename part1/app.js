b99e033394f5f137e30ab92f91671b8f654d5f32
yvnAK <57645366+yvnAK@users.noreply.github.com>
2025-06-21 00:20:10 +0000

    [automatic commit] 2025-06-21 00:20:10 UTC 59.102.38.160

--- part1/app.js
+++ part1/app.js
@@ -94,6 +94,17 @@ let db;
             )
         `);
         // =================================================
+
+        // Insert data if table is empty
+        const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
+        if (rows[0].count === 0) {
+            await db.execute(`
+        INSERT INTO books (title, author) VALUES
+        ('1984', 'George Orwell'),
+        ('To Kill a Mockingbird', 'Harper Lee'),
+        ('Brave New World', 'Aldous Huxley')
+      `);
+        }
     } catch (err) {
         console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
     }
