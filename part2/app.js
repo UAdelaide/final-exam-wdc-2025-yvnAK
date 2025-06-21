7054a067a7edfc2d7c587e23620b66020fa8efd2
yvnAK <57645366+yvnAK@users.noreply.github.com>
2025-06-21 06:45:24 +0000

    [automatic commit] 2025-06-21 06:45:24 UTC 59.102.38.160

--- part2/app.js
+++ part2/app.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    
+    saveUninitialized: false,
+    cookie: { maxAge: 24 * 60 * 60 * 1000 }
 }));
 
 // Middleware
