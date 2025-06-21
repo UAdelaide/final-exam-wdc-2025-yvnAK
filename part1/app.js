fdd7bef468335476ab6ae409d693c07d16ad4d29
yvnAK <57645366+yvnAK@users.noreply.github.com>
2025-06-21 04:05:40 +0000

    [automatic commit] 2025-06-21 04:05:40 UTC 59.102.38.160

--- part1/app.js
+++ part1/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
 var mysql = require('mysql2/promise');
 
 var app = express();
