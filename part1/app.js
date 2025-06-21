d9a86013eb859ace1d8df979537b7309768ea478
yvnAK <57645366+yvnAK@users.noreply.github.com>
2025-06-21 00:20:51 +0000

    [automatic commit] 2025-06-21 00:20:51 UTC 59.102.38.160

--- part1/app.js
+++ part1/app.js
@@ -95,14 +95,14 @@ let db;
         `);
         // =================================================
 
-        // Insert data if table is empty
+        // ====================================
         const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
         if (rows[0].count === 0) {
             await db.execute(`
                 INSERT INTO books (title, author) VALUES
                 ('1984', 'George Orwell'),
-        ('To Kill a Mockingbird', 'Harper Lee'),
-        ('Brave New World', 'Aldous Huxley')
+                ('To Kill a Mockingbird', 'Harper Lee'),
+                ('Brave New World', 'Aldous Huxley')
       `);
         }
     } catch (err) {
