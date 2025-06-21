0adc385b5473d61c3e4875bc5af4b1498f0a805e
yvnAK <57645366+yvnAK@users.noreply.github.com>
2025-06-21 00:47:58 +0000

    [automatic commit] 2025-06-21 00:47:57 UTC 59.102.38.160

--- part1/app.js
+++ part1/app.js
@@ -140,6 +140,7 @@ let db;
 app.get('/', async (req, res) => {
     try {
         // get all dog names + sizes and their owner username
+        // owner username is from the Users table
         const [dogRows] = await db.execute();
         res.json(dogRows);
     } catch (err) {
