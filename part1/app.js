87aad9ede2a4c648f1756104b034aee95f339a49
yvnAK <57645366+yvnAK@users.noreply.github.com>
2025-06-21 00:12:59 +0000

    [automatic commit] 2025-06-21 00:12:59 UTC 59.102.38.160

--- part1/app.js
+++ part1/app.js
@@ -56,11 +56,14 @@ let db;
         `);
         await db.execute(`
             create table if not exists WalkRequests (
-                dog_id INT AUTO_INCREMENT PRIMARY KEY,
-                owner_id INT NOT NULL,
-                name VARCHAR(50) NOT NULL,
-                size ENUM('small', 'medium', 'large') NOT NULL,
-                FOREIGN KEY (owner_id) REFERENCES Users(user_id)
+                request_id INT AUTO_INCREMENT PRIMARY KEY,
+    dog_id INT NOT NULL,
+    requested_time DATETIME NOT NULL,
+    duration_minutes INT NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    status ENUM('open', 'accepted', 'completed', 'cancelled') DEFAULT 'open',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (dog_id) REFERENCES Dogs(dog_id)
             )
         `);
         await db.execute(`
