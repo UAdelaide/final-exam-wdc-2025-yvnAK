b2aee65c333ca55011b4daa7544b8560580a0a52
yvnAK <57645366+yvnAK@users.noreply.github.com>
2025-06-21 00:20:30 +0000

    [automatic commit] 2025-06-21 00:20:30 UTC 59.102.38.160

--- part1/app.js
+++ part1/app.js
@@ -99,8 +99,8 @@ let db;
         const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
         if (rows[0].count === 0) {
             await db.execute(`
-        INSERT INTO books (title, author) VALUES
-        ('1984', 'George Orwell'),
+                INSERT INTO books (title, author) VALUES
+                ('1984', 'George Orwell'),
         ('To Kill a Mockingbird', 'Harper Lee'),
         ('Brave New World', 'Aldous Huxley')
       `);
