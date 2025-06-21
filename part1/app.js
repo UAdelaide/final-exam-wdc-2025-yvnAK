9d5f6ead0ccbcc483df99422dd5cf84d5d502e6e
yvnAK <57645366+yvnAK@users.noreply.github.com>
2025-06-21 00:44:50 +0000

    [automatic commit] 2025-06-21 00:44:50 UTC 59.102.38.160

--- part1/app.js
+++ part1/app.js
@@ -163,7 +163,7 @@ app.get('/', async (req, res) => {
 app.get('/', async (req, res) => {
     try {
         // get users where role = 'walker'
-        // list username, number of ratings, rating, 
+        // list username, number of ratings, rating, completed walks
         const [userRows] = await db.execute();
         res.json(userRows);
     } catch (err) {
